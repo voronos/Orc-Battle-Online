012091083163617eff38e837880a51cbdf5bfad7
Mark Nelson <nels2426@gmail.com>
2011-04-09 13:07:42 -0500

    adding some todos

--- src/orc_battle_online/stateful_links.clj
+++ src/orc_battle_online/stateful_links.clj
@@ -2,7 +2,8 @@
   (:use (hiccup core)))
 
 (def *link-map* (ref {}))
-
+;; TODO figure out a way to clean up the link-map from time to time
+;; TODO add validation function to ensure that only functions with an arity of one are added as values
 (defn create-link [text fun]
   (dosync
    (alter *link-map* assoc (hash fun) fun)
